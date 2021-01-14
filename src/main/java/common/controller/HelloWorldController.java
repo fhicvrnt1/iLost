3ba3383204c6bc21a7eb7598e481db13c86eb1f7
fhicvrnt1 <woaini582064@163.com>
2021-01-14 14:13:21 +0800

    使用 Joda-Time 管理时间 1.使用 Joda-Time 包管理时间相关方法 2.优化HelloWorld范例的controller

--- src/main/java/common/controller/HelloWorldController.java
+++ src/main/java/common/controller/HelloWorldController.java
@@ -6,26 +6,32 @@ package common.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.Controller;
 
 /**
- * @Description 
+ * @Description
  * @author 可凡
  * @date 2013年12月11日 下午2:06:16
  * @mail liukefan@gmail.com
  * @version v1.0.0
  */
-public class HelloWorldController extends AbstractController{
+public class HelloWorldController implements Controller {
 
 	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
-		ModelAndView model = new ModelAndView("HelloWorldPage");
-		model.addObject("msg", "hello world");
-		
-		return model;
+		ModelAndView mv = new ModelAndView();
+		String mvMsgFlg = "msg";
+		String mvMsg = "hello world!";
+		String mvName = "HelloWorldPage";
+		DateTime date = new DateTime();
+		mvMsg += " " + date.toString("yyyy/MM/dd HH:mm:ss EE");
+		//System.out.println(mvMsgFlg + " " + mvMsg + " " + mvName);
+		mv.addObject(mvMsgFlg, mvMsg);
+		mv.setViewName(mvName);
+		return mv;
 	}
-	
+
 }
