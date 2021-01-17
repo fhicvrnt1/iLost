7ffee6981c15c510b20d3f44e8346c2e568bb227
fhicvrnt1 <woaini582064@163.com>
2021-01-17 23:46:12 +0800

    使用注解方式实现spring-mvc

--- src/main/java/common/controller/HelloWorldController.java
+++ src/main/java/common/controller/HelloWorldController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.joda.time.DateTime;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -17,11 +19,11 @@ import org.springframework.web.servlet.mvc.AbstractController;
  * @mail liukefan@gmail.com
  * @version v1.0.0
  */
-public class HelloWorldController extends AbstractController {
+@Controller
+public class HelloWorldController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/hello")
+	public ModelAndView helloWorld() {
 		// 1、收集参数
 		// 2、绑定参数到命令对象
 		// 3、调用业务对象
