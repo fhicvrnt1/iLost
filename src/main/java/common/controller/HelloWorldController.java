866c321cce7edeb31376aee0de3ab2838d2aede7
fhicvrnt1 <woaini582064@163.com>
2021-01-22 11:54:41 +0800

    使用 Spring 的 RequestParam 绑定 URL 参数

--- src/main/java/common/controller/HelloWorldController.java
+++ src/main/java/common/controller/HelloWorldController.java
@@ -3,14 +3,17 @@
  */
 package common.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+
+//import org.springframework.web.servlet.mvc.AbstractController;
 
 /**
  * @Description
@@ -20,9 +23,10 @@ import org.springframework.web.servlet.mvc.AbstractController;
  * @version v1.0.0
  */
 @Controller
+@RequestMapping(value = "/hello/**")
 public class HelloWorldController {
 
-	@RequestMapping(value = "/hello")
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public ModelAndView helloWorld() {
 		// 1、收集参数
 		// 2、绑定参数到命令对象
@@ -31,6 +35,26 @@ public class HelloWorldController {
 		ModelAndView mv = new ModelAndView();
 		String mvMsgFlg = "msg";
 		String mvMsg = "hello world!";
+		String mvName = "HelloWorldPage";
+		DateTime date = new DateTime();
+		mvMsg += " " + date.toString("yyyy/MM/dd HH:mm:ss EE");
+		mv.addObject(mvMsgFlg, mvMsg);
+		mv.setViewName(mvName);
+		return mv;
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public ModelAndView helloUser(
+			@RequestParam(value = "username", required = true) String username) {
+		
+		// 1、收集参数
+		// 2、绑定参数到命令对象
+		// 3、调用业务对象
+		// 4、选择下一个页面
+
+		ModelAndView mv = new ModelAndView();
+		String mvMsgFlg = "msg";
+		String mvMsg = "hello " + username;
 		String mvName = "HelloWorldPage";
 		DateTime date = new DateTime();
 		mvMsg += " " + date.toString("yyyy/MM/dd HH:mm:ss EE");
