430721af487b0926463623dda4afb90dcc08fd89
fhicvrnt1 <woaini582064@163.com>
2021-02-03 16:55:01 +0800

    update

---
+++ src/main/java/me/second_life/ilost/repository/IUserRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package me.second_life.ilost.repository;
+
+
+
+/**
+ * @Description
+ * @author 可凡
+ * @date 2013年12月27日 下午3:49:32
+ * @mail
+ * @version v1.0.0
+ */
+public interface IUserRepository<T> extends Repository<T> {
+
+}
---
+++ src/main/java/me/second_life/ilost/service/impl/UserServiceImpl.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package me.second_life.ilost.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import me.second_life.ilost.pojo.User;
+import me.second_life.ilost.repository.impl.UserRepositoryImpl;
+import me.second_life.ilost.service.IUserService;
+
+/**
+ * @Description
+ * @author 可凡
+ * @date 2013年12月29日 下午10:45:57
+ * @mail
+ * @version v1.0.0
+ */
+@Service
+public class UserServiceImpl implements IUserService {
+
+	@Autowired
+	private UserRepositoryImpl userRepository;
+
+	@Override
+	public boolean addUser(User user) {
+		return false;
+//		return userRepository.add(user);
+	}
+
+	@Override
+	public List<User> getUsers() {
+		return null;
+//		List<User> users = userRepository.query(
+//				"select * from user order by id", new Object[] { 17 });
+//		// for (User user : users) {
+//		// System.out.println(user.getUser_Name() + "    "
+//		// + user.getUser_Password());
+//		// }
+//		return users;
+	}
+
+	@Override
+	public boolean getUser(String username, String password) {
+		return false;
+//		boolean found = false;
+//		List<User> users = userRepository.query(
+//				"select * from user order by id where user_name = '" + username
+//						+ "' and password = '" + password + "'",
+//				new Object[] { 17 });
+//		if (users.size() > 0) {
+//			found = true;
+//		}
+//		return found;
+	}
+
+}
